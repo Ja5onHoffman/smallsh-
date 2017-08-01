a15391cd5880d8ebd87de5a50626c6cea750b70c
Ja5onHoffman <j@sonhoffman.com>
2017-08-01 13:05:33 -0400

    SIGINT ignore and status

--- smallsh.c
+++ smallsh.c
@@ -5,9 +5,12 @@
 #include <dirent.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <sys/wait.h>
 #include <unistd.h>
 #include <pthread.h>
 #include <time.h>
+#include <signal.h>
+#include <assert.h>
 
 
 
@@ -17,16 +20,37 @@ void catchSIGINT(int signo)
     write(STDOUT_FILENO, message, 28);
 }
 
+pid_t parent_pid;
+
+void sigquit_handler (int sig) {
+    assert(sig == SIGQUIT);
+    pid_t self = getpid();
+    printf("pid: %d\n", self);
+    if (parent_pid != self) _exit(0);
+}
+
+void getStatus(int *childExitMethod);
+
+
 char* removeNewline(char *str);
 void execute(char** argv);
 
 int main()
 {
+    // struct sigaction SIGINT_ignore = {0};
+    // SIGINT_ignore.sa_handler = SIG_IGN;
+    // sigaction(SIGINT, &SIGINT_ignore, NULL);
     struct sigaction SIGINT_action = {0};
     SIGINT_action.sa_handler = catchSIGINT;
     sigfillset(&SIGINT_action.sa_mask);
-    //SIGINT_action.sa_flags = SA_RESTART;
+    SIGINT_action.sa_flags = SA_RESTART;
     sigaction(SIGINT, &SIGINT_action, NULL);
+
+    // struct sigaction SIGQUIT_action = {0};
+    // SIGQUIT_action.sa_handler = sigquit_handler;
+    // sigfillset(&SIGQUIT_action.sa_mask);
+    // sigaction(SIGQUIT, &SIGINT_action, NULL);
+
 
     int numCharsEntered = -5;       // How many chars we entered
     int currChar = -5;              // Tracks where we are when we print out every char
@@ -34,24 +58,22 @@ int main()
     char* lineEntered = NULL;       // Points to a buffer allocated by getline() that holds our entered string + \n + \0
     char* parsedArgs[512];
     size_t MAX_LINE = 256;
-
-
+    pid_t topPid = getpid();
+    int childExitMethod;
     // // Init array of pointers to strings
     // for (int i = 0; i < MAX_LINE; i++) {
     //     parsedArgs[i] = (char*)malloc(MAX_LINE);
     // }
 
-    while(1)
-    {
+    while(1) {
         // Get input from the user
-        while(1)
-        {
+        while(1) {
             // Init array of pointers to strings
             for (int i = 0; i < MAX_LINE; i++) {
                 parsedArgs[i] = (char*)malloc(MAX_LINE);
             }
 
-            printf("smallsh: ");
+            printf(": ");
             // Get a line from the user
             fflush(stdin);
             numCharsEntered = getline(&lineEntered, &bufferSize, stdin);
@@ -63,7 +85,7 @@ int main()
                 int c = 0;
                 char *tok = strtok(lineEntered, " ");
                 while(tok) {
-                    //                    memcpy(&parsedArgs[c], tok, sizeof(tok) / sizeof(char));
+                    // memcpy(&parsedArgs[c], tok, sizeof(tok) / sizeof(char));
                     strcpy(parsedArgs[c], tok);
                     tok = strtok(NULL, " ");
                     c++;
@@ -78,63 +100,60 @@ int main()
                     break;
                 }
             }
-
-
-
         }
 
         // MARK: cd command
-        if (!strcmp(parsedArgs[0], "cd")) {
+        if (strcmp(parsedArgs[0], "cd") == 0) {
+          pid_t spawnPid = -5;
+          // int childExitMethod;
+          spawnPid = fork();
+          // If child run cd
+          if (spawnPid == 0) {
             char currentDir[1024];
             char *home = getenv("HOME");
             // If no args go to $HOME
-            if (*parsedArgs[1] == '\0') {
+            if (!parsedArgs[1]) {
                 chdir(home);
                 getcwd(currentDir, sizeof(currentDir));
                 printf("%s\n", currentDir);
             } else {
                 int r;
                 removeNewline(parsedArgs[1]);
-                r = chdir(parsedArgs[1]);
-                // If successful
-                if (r >=0) {
-                    getcwd(currentDir, sizeof(currentDir));
-                    printf("%s\n", currentDir);
-                } else {
-//                  printf("%d\n", strerr(errno));
-                    printf("err\n");
+                if (!chdir(parsedArgs[1])) {
+                  exit(1);
                 }
             }
+            exit(0); // Successful exit
+
+          // Else wait for cd to run
+          } else {
+            waitpid(spawnPid, &childExitMethod, 0);
+          }
 
 
         // MARK: status command
+        // This one does't run in background
         } else if (!strcmp(parsedArgs[0], "status")) {
-
-            // Built in commands do not have to support exit status
-
-
+          getStatus(&childExitMethod);
 
         // MARK: exit command
         } else if (!strcmp(parsedArgs[0], "exit")) {
 
-          int childExitMethod;
-          pid_t childPid = wait(NULL);e
-          exit(0);
-
         } else {
             pid_t spawnPid = -5;
-            int childExitStatus = -5;
-
-            // Is this leaving a zombie
             spawnPid = fork();
             if (spawnPid == 0) {
               execute(parsedArgs);
-            }
+            } else {
+                waitpid(spawnPid, &childExitMethod, 0);
+                if (WIFSIGNALED(childExitMethod)) {
+                  getStatus(&childExitMethod);
+                }
 
-            // Wait to terminate
-            pid_t actualPid = waitpid(spawnPid, &childExitStatus, 0);
+            }
         }
     }
+
 // TAG: atexit() can clean up the child processes
 
 
@@ -167,6 +186,15 @@ char* removeNewline(char *str) {
     return str;
 }
 
+void getStatus(int *childExitMethod) {
+  int exitStatus;
+  if (WIFEXITED(*childExitMethod)) {
+    exitStatus = WEXITSTATUS(*childExitMethod);
+    printf("exit value %d\n", exitStatus);
+  } else if (WIFSIGNALED(*childExitMethod)) {
+    printf("terminated by signal %d\n", WTERMSIG(*childExitMethod));
+  }
+}
 
 
 //

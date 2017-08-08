84e2a0a67f0bb8f6ac13d553ea0044a2cfb1f165
Ja5onHoffman <j@sonhoffman.com>
2017-08-08 09:37:25 -0500

    file problems

--- smallsh.c
+++ smallsh.c
@@ -35,22 +35,22 @@ void sigquit_handler (int sig) {
 }
 
 void sigchld_handler(int sig) {
-  // int status;
-  // int saved_errno = errno;
-  // waitpid(-1, &status, WNOHANG) > 0) {}
-  bgQuit = 1;
-  // errno = saved_errno;
-  // write(STDOUT_FILENO, &bg, 2);
-  // if (bg) {
-  //   char *backgroundpid = "background pid";
-  //   char *isdone = " is done: exit value";
-  //   pid_t p = getpid();
-  //   int s = WIFEXITED(status);
-  //   write(STDOUT_FILENO, backgroundpid,14);
-  //   write(STDOUT_FILENO, &p, sizeof(p));
-  //   write(STDOUT_FILENO, isdone, 20);
-  //   write(STDOUT_FILENO, &s, sizeof(s));
-  // }
+    // int status;
+    // int saved_errno = errno;
+    // waitpid(-1, &status, WNOHANG) > 0) {}
+    bgQuit = 1;
+    // errno = saved_errno;
+    // write(STDOUT_FILENO, &bg, 2);
+    // if (bg) {
+    //   char *backgroundpid = "background pid";
+    //   char *isdone = " is done: exit value";
+    //   pid_t p = getpid();
+    //   int s = WIFEXITED(status);
+    //   write(STDOUT_FILENO, backgroundpid,14);
+    //   write(STDOUT_FILENO, &p, sizeof(p));
+    //   write(STDOUT_FILENO, isdone, 20);
+    //   write(STDOUT_FILENO, &s, sizeof(s));
+    // }
 }
 
 void getStatus(int *childExitMethod);
@@ -97,7 +97,7 @@ int main()
     pid_t childPid;
     pid_t bgPid = -5;
     int childExitMethod;
-    int io, bg; // Flags for redirection and background
+    int io = 0, bg = 0; // Flags for redirection and background
     char in[256], out[256];
 
     // // Init array of pointers to strings
@@ -119,16 +119,16 @@ int main()
             }
 
             if (bgQuit && bg) {
-              // Clean up bg process
-              waitpid(bgPid, &childExitMethod, 0);
-              printf("background pid %d is done: ", bgPid);
-              fflush(stdout);
-              int exitStatus = WEXITSTATUS(childExitMethod);
-              if (WIFEXITED(childExitMethod)) {
-                printf("exit value %d\n", exitStatus);
-              } else {
-                printf("terminated by a signal %d\n", exitStatus);
-              }
+                // Clean up bg process
+                waitpid(bgPid, &childExitMethod, 0);
+                printf("background pid %d is done: ", bgPid);
+                fflush(stdout);
+                int exitStatus = WEXITSTATUS(childExitMethod);
+                if (WIFEXITED(childExitMethod)) {
+                    printf("exit value %d\n", exitStatus);
+                } else {
+                    printf("terminated by a signal %d\n", exitStatus);
+                }
                 fflush(stdout);
                 // Reset bgquit and bg
                 bgQuit = 0;
@@ -162,8 +162,8 @@ int main()
                 if (*parsedArgs[0] == '#') {
                     continue;
                 } else {
-                  rdbg(parsedArgs, c, &io, &bg, in, out);
-                  break;
+                    rdbg(parsedArgs, c, &io, &bg, in, out);
+                    break;
                 }
             }
             // rdbg(parsedArgs, c, &io, &bg, in, out);
@@ -173,63 +173,63 @@ int main()
         spawnPid = fork();
 
         if (spawnPid == 0) {
-          if (io) {
-            redirect(in, out, io);
-          }
+            if (io) {
+                redirect(in, out, io);
+            }
 
-          // if (bg) {
-          //   bgPid = getpid();
-          // }
-
-          if (!strcmp(parsedArgs[0], "cd")) {
-              char currentDir[1024];
-              char *home = getenv("HOME");
-              // If no args go to $HOME
-              if (!parsedArgs[1]) {
-                  chdir(home);
-                  getcwd(currentDir, sizeof(currentDir));
-                  printf("%s\n", currentDir);
-                  fflush(stdout);
-              } else {
-                  int r;
-                  removeNewline(parsedArgs[1]);
-                  chdir(parsedArgs[1]);
-                  getcwd(currentDir, sizeof(currentDir));
-                  printf("%s\n", currentDir);
-                  fflush(stdout);
-              }
-
-          // MARK: status command
-          // This one does't run in background
-          } else if (!strcmp(parsedArgs[0], "status")) {
-            getStatus(&childExitMethod);
-          // MARK: exit command
-          } else if (!strcmp(parsedArgs[0], "exit")) {
-            // int t = 0;
-            // while (!t) {
-            //   t = waitpid(-1, &childExitMethod, WNOHANG);
+            // if (bg) {
+            //   bgPid = getpid();
             // }
-            // kill(parent_pid, SIGTERM);
-            raise(SIGTERM);
 
-          } else {
-              execute(parsedArgs);
-          }
+            if (!strcmp(parsedArgs[0], "cd")) {
+                char currentDir[1024];
+                char *home = getenv("HOME");
+                // If no args go to $HOME
+                if (!parsedArgs[1]) {
+                    chdir(home);
+                    getcwd(currentDir, sizeof(currentDir));
+                    printf("%s\n", currentDir);
+                    fflush(stdout);
+                } else {
+                    int r;
+                    removeNewline(parsedArgs[1]);
+                    chdir(parsedArgs[1]);
+                    getcwd(currentDir, sizeof(currentDir));
+                    printf("%s\n", currentDir);
+                    fflush(stdout);
+                }
+
+                // MARK: status command
+                // This one does't run in background
+            } else if (!strcmp(parsedArgs[0], "status")) {
+                getStatus(&childExitMethod);
+                // MARK: exit command
+            } else if (!strcmp(parsedArgs[0], "exit")) {
+                // int t = 0;
+                // while (!t) {
+                //   t = waitpid(-1, &childExitMethod, WNOHANG);
+                // }
+                // kill(parent_pid, SIGTERM);
+                raise(SIGTERM);
+
+            } else {
+                execute(parsedArgs);
+            }
         } else {
-          // If not a background process block
-          if (!bg) {
-            waitpid(spawnPid, &childExitMethod, 0);
-            if (WIFSIGNALED(childExitMethod)) {
-              getStatus(&childExitMethod);
+            // If not a background process block
+            if (!bg) {
+                waitpid(spawnPid, &childExitMethod, 0);
+                if (WIFSIGNALED(childExitMethod)) {
+                    getStatus(&childExitMethod);
+                }
+            } else {
+                if (bg) {
+                    bgPid = spawnPid;
+                    printf("background pid is %d\n", spawnPid);
+                    fflush(stdout);
+                }
+                // waitbg(spawnPid, &childExitMethod);
             }
-          } else {
-            if (bg) {
-              bgPid = spawnPid;
-              printf("background pid is %d\n", spawnPid);
-              fflush(stdout);
-            }
-            // waitbg(spawnPid, &childExitMethod);
-          }
         }
     }
 
@@ -238,10 +238,10 @@ int main()
 
 // Execute args (taken from lecture slide)
 void execute(char** argv) {
-  if (execvp(*argv, argv) < 0) {
+    if (execvp(*argv, argv) < 0) {
         printf("execvp failed\n");
         exit(1);
-  }
+    }
 }
 
 // Helper function to remove a new line from character strings
@@ -254,46 +254,50 @@ char* removeNewline(char *str) {
 }
 
 void getStatus(int *childExitMethod) {
-  int exitStatus;
-  if (WIFEXITED(*childExitMethod)) {
-    exitStatus = WEXITSTATUS(*childExitMethod);
-    printf("exit value %d\n", exitStatus);
-    fflush(stdout);
-  } else if (WIFSIGNALED(*childExitMethod)) {
-    printf("terminated by signal %d\n", WTERMSIG(*childExitMethod));
-    fflush(stdout);
-  }
+    int exitStatus;
+    if (WIFEXITED(*childExitMethod)) {
+        exitStatus = WEXITSTATUS(*childExitMethod);
+        printf("exit value %d\n", exitStatus);
+        fflush(stdout);
+    } else if (WIFSIGNALED(*childExitMethod)) {
+        printf("terminated by signal %d\n", WTERMSIG(*childExitMethod));
+        fflush(stdout);
+    }
 }
 
 void redirect(char *in, char *out, int io) {
-  int file_out = 0, file_in = 0;
-  int res;
-  switch(io) {
-    case 0:
-      break;
-    case 1:
-      file_out = open(out, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-      if (file_out < 0) {
-        exit(1);
-      }
-      dup2(file_out, STD_OUT);
-    case 2:
-      file_in = open(in, O_RDONLY);
-      printf("%d\n", file_in);
-      if (file_in < 0) {
-        printf("cannot open file for input\n");
-        fflush(stdout);
-        exit(1);
-      }
-      dup2(file_in, STD_IN);
-    case 3:
-      file_in = open(in, O_RDONLY, 0644);
-      file_out = open(out, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+    int file_out = 0, file_in = 0;
+    int res;
+    switch(io) {
+        case 0:
+            break;
+        case 1:
+            file_out = open(out, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+            fcntl(file_out, F_SETFD, FD_CLOEXEC);
+            if (file_out < 0) {
+                return;
+            } else {
+                dup2(file_out, STD_OUT);
+            }
+        case 2:
+            file_in = open(in, O_RDONLY, 0644);
+            fcntl(file_in, F_SETFD, FD_CLOEXEC);
+            if (file_in < 0) {
+                printf("cannot open file for input\n");
+                // close(file_in);
+                fflush(stdout);
+                return;
+            } else {
+                dup2(file_in, STD_IN);
+            }
+        case 3:
+            file_in = open(in, O_RDONLY, 0644);
+            file_out = open(out, O_WRONLY | O_CREAT | O_TRUNC, S_IRWXU | S_IRWXG);
 
-      // Need to handle error here as well
-      dup2(file_in, STD_IN);
-      dup2(file_out, STD_OUT);
-  }
+            // Need to handle error here as well
+            dup2(file_in, STD_IN);
+            dup2(file_out, STD_OUT);
+    }
 }
 
 int waitbg(int childPid, int *childExitMethod) {
@@ -304,45 +308,45 @@ int waitbg(int childPid, int *childExitMethod) {
 
 // Parses input to handle background and redirection
 void rdbg(char **args, int len, int *io, int *bg, char *in, char *out) {
-  int clean = 0, ig = 0;
-  ig = (!strcmp(args[0], "status") || !strcmp(args[0], "exit") || !strcmp(args[0], "cd"));
-  if (*args[len-1] == '&' && !ig) {
+    int clean = 0, ig = 0;
+    ig = (!strcmp(args[0], "status") || !strcmp(args[0], "exit") || !strcmp(args[0], "cd"));
+    if (*args[len-1] == '&' && !ig) {
         *bg = 1; clean = 1;
         // i/o set to /dev/null in case no i/o inputs
         strcpy(in, "/dev/null");
         strcpy(out, "/dev/null");
-  }
-
-  int i;
-  for (i = 0; i < len; i++) {
-    if (*args[i] == '>') {
-      *io = 1; clean = 1;
-      strcpy(out, args[i+1]);
-    } else if (*args[i] == '<') {
-      *io = 2; clean = 1;
-      strcpy(in, args[i+1]);
     }
-  }
-  // If input and output io flag = 3;
-  if (*out && *in) { *io = 3; };
 
-  if (clean) {
     int i;
     for (i = 0; i < len; i++) {
-      char c = *args[i];
-      if (c == '>' || c == '<' || c == '&') {
-        for (; i < len; i++) {
-          args[i] = (char*)0;
+        if (*args[i] == '>') {
+            *io = 1; clean = 1;
+            strcpy(out, args[i+1]);
+        } else if (*args[i] == '<') {
+            *io = 2; clean = 1;
+            strcpy(in, args[i+1]);
         }
-        break;
-      }
     }
-  }
+    // If input and output io flag = 3;
+    if (*out && *in) { *io = 3; };
+
+    if (clean) {
+        int i;
+        for (i = 0; i < len; i++) {
+            char c = *args[i];
+            if (c == '>' || c == '<' || c == '&') {
+                for (; i < len; i++) {
+                    args[i] = (char*)0;
+                }
+                break;
+            }
+        }
+    }
 }
 
 void traceParsedArgs(char** args) {
-  int i;
-  for (i = 0; i < 3; i++) {
-    printf("%s\n", args[i]);
-  }
+    int i;
+    for (i = 0; i < 3; i++) {
+        printf("%s\n", args[i]);
+    }
 }
